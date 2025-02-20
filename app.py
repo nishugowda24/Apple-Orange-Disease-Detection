c7145c8bf4b3f49a63ba767497481393ae30d792
nishugowda24 <nishugowda.c@gmail.com>
2025-02-20 22:04:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,152 @@
+import os
+from flask import Flask, request, render_template, send_from_directory,session
+import mysql.connector
+
+
+app = Flask(__name__)
+# app = Flask(__name__, static_folder="images")
+
+
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+classes = ['Apple Blotch' ,'Apple Rot' , 'Apple Scab' , 'Fresh Apple' , 'Fresh Orange' , 'Orange Rot'] 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/user")
+def user():
+    return render_template("user.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route('/registration')
+def registration():
+    return render_template("ureg.html",msg='Successfully Registered!!')
+
+@app.route('/userlog',methods=['POST', 'GET'])
+def userlog():
+    global name, password
+    global user
+
+    if request.method == "POST":
+
+        username = request.form['email']
+        password1 = request.form['pass']
+        print('p')
+        mydb = mysql.connector.connect(host="localhost",user="root",passwd="root",port=3306,database="fruit")
+        cursor = mydb.cursor()
+        sql = "select * from ureg where email='%s' and pass='%s'" % (username, password1)
+        print('q')
+        x = cursor.execute(sql)
+        print(x)
+        results = cursor.fetchall()
+        print(results)
+        if len(results) > 0:
+            print('r')
+            #session['user'] = username
+            #session['id'] = results[0][0]
+            #print(id)
+            #print(session['id'])
+            return render_template('userhome.html', msg="Login Success")
+        else:
+            return render_template('user.html', msg="Login Failure!!!")
+
+    return render_template('user.html')
+
+
+@app.route('/uregback',methods=['POST','GET'])
+def uregback():
+    if request.method=='POST':
+        name=request.form['name']
+        email=request.form['email']
+        pwd=request.form['pass']
+        ph=request.form['ph']
+        gender=request.form['gender']
+
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="root",
+            port=3306,
+            database="fruit"
+        )
+        mycursor = mydb.cursor()
+
+        sql = "INSERT INTO ureg (name,email,pass,ph,gender) VALUES (%s, %s,%s,%s,%s)"
+        val = (name,email,pwd,ph,gender)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    return render_template('user.html')
+    print("Successfully Registered")
+
+@app.route('/upload1')
+def upload1():
+    return render_template("upload.html",msg='Successfully logined!!')
+
+@app.route('/userhome')
+def userhome():
+    return render_template("userhome.html",msg='Successfully logined!!')
+
+
+
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    # print('a')
+    # target = os.path.join(APP_ROOT, 'images/')
+    # # target = os.path.join(APP_ROOT, 'static/')
+    # print(target)
+    # if not os.path.isdir(target):
+    #         os.mkdir(target)
+    # else:
+    #     print("Couldn't create upload directory: {}".format(target))
+    # print(request.files.getlist("file"))
+    # for upload in request.files.getlist("file"):
+    #     print(upload)
+    #     print("{} is the file name".format(upload.filename))
+    #     filename = upload.filename
+    #     destination = "/".join([target, filename])
+    #     print ("Accept incoming file:", filename)
+    #     print ("Save it to:", destination)
+    #     upload.save(destination)
+    if request.method=="POST":
+        myfile = request.files['file']
+        fn = myfile.filename
+        mypath = os.path.join('images/', fn)
+        myfile.save(mypath)
+
+        print("{} is the file name", fn)
+        print("Accept incoming file:", fn)
+        print("Save it to:", mypath)
+
+        #import tensorflow as tf
+        import numpy as np
+        from tensorflow.keras.preprocessing import image
+
+        from tensorflow.keras.models import load_model
+        new_model = load_model('alg/MobileeNt.h5')
+        new_model.summary()
+        test_image = image.load_img(mypath, target_size=(224, 224))
+        test_image = image.img_to_array(test_image)
+        test_image=test_image/255
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = new_model.predict(test_image)
+       # prediction=classes[np.argmax(result)]
+
+        prediction=classes[np.argmax(result)]
+
+    #return send_from_directory("images", filename, as_attachment=True)
+    return render_template("template.html",image_name=fn, text=prediction)
+
+@app.route('/upload/<filename>')
+def send_image(filename):
+    return send_from_directory("images", filename)
+
+if __name__ == "__main__":
+    app.run(debug=False, threaded=False)
+
---
+++ database.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Enterprise - MySQL GUI v6.56
+MySQL - 5.5.5-10.1.13-MariaDB : Database - fruit
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`fruit` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `fruit`;
+
+/*Table structure for table `ureg` */
+
+DROP TABLE IF EXISTS `ureg`;
+
+CREATE TABLE `ureg` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `pass` varchar(100) DEFAULT NULL,
+  `ph` varchar(100) DEFAULT NULL,
+  `gender` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `ureg` */
+
+insert  into `ureg`(`id`,`name`,`email`,`pass`,`ph`,`gender`) values (1,'lakshmi','lakshmi@gmail.com','1','98989898989','female');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
